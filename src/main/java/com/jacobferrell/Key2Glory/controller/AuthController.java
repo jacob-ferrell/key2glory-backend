fbc2bba0150cdc8a33f2e2462a67316e7a4451c6
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-30 22:21:48 -0400

    Simple controller for testing public/private routes

--- src/main/java/com/jacobferrell/Key2Glory/controller/AuthController.java
+++ src/main/java/com/jacobferrell/Key2Glory/controller/AuthController.java
@@ -1,25 +1,14 @@
 package com.jacobferrell.Key2Glory.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Map;
-
 @RestController
 @RequestMapping(path= "api")
 @CrossOrigin(origins = "http://localhost:5174", allowedHeaders = "*")
 public class AuthController {
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
     @GetMapping("/public/hello")
     public String hello() {
         return "Hello!";
