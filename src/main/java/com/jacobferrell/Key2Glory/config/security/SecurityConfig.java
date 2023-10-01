c2f6374cfcf5cfebeb9ee4b866db28a78625d2ee
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-09-30 22:15:54 -0400

    Implemented properly functioning SecurityConfig with auth0

--- src/main/java/com/jacobferrell/Key2Glory/config/security/SecurityConfig.java
+++ src/main/java/com/jacobferrell/Key2Glory/config/security/SecurityConfig.java
@@ -1,32 +1,54 @@
 package com.jacobferrell.Key2Glory.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import java.util.Arrays;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthenticationErrorHandler authenticationErrorHandler;
+    @Autowired
+    private AuthenticationErrorHandler authenticationErrorHandler;
+    private static final String API_URL_PATTERN = "/api/public/**";
 
-    public SecurityConfig(AuthenticationErrorHandler authenticationErrorHandler) {
-        this.authenticationErrorHandler = authenticationErrorHandler;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5174", "http://localhost:8080"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
-    public SecurityFilterChain httpSecurity(final HttpSecurity http) throws Exception {
+    public SecurityFilterChain httpSecurity(final HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+
         return http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/private/**").authenticated()
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console(), mvcMatcherBuilder.pattern("/api/private/**")).authenticated()
                         .anyRequest().permitAll())
                 .cors(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
@@ -34,4 +56,5 @@ public class SecurityConfig {
                         .authenticationEntryPoint(authenticationErrorHandler))
                 .build();
     }
+
 }
