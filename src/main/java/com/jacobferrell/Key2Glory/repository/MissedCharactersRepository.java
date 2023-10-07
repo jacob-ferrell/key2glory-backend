88fdd5440382a8f2c3a9f7191663198c34834ca6
Jacob Ferrell <michaeljacobferrell@gmail.com>
2023-10-07 12:13:21 -0400

    removed unused imports and commented code

--- src/main/java/com/jacobferrell/Key2Glory/repository/MissedCharactersRepository.java
+++ src/main/java/com/jacobferrell/Key2Glory/repository/MissedCharactersRepository.java
@@ -2,13 +2,11 @@ package com.jacobferrell.Key2Glory.repository;
 
 import com.jacobferrell.Key2Glory.model.MissedCharacters;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MissedCharactersRepository extends JpaRepository<MissedCharacters, Long> {
-    //@Query("SELECT m FROM MissedCharacters m WHERE m.username = :username")
     Optional<MissedCharacters> findByUsername(String username);
 }
