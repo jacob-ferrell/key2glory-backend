f525d23396499b8145d7e0d59d2a4bd927797979
Jacob Ferrell <michaeljacobferrell@gmail.com>
2024-10-05 10:29:35 -0400

    adds findByType and findAll methods

--- src/main/java/com/jacobferrell/Key2Glory/repository/TypingTestRepository.java
+++ src/main/java/com/jacobferrell/Key2Glory/repository/TypingTestRepository.java
@@ -1,9 +1,11 @@
 package com.jacobferrell.Key2Glory.repository;
 
-import com.jacobferrell.Key2Glory.model.Score;
 import com.jacobferrell.Key2Glory.model.TypingTest;
+import com.jacobferrell.Key2Glory.model.TypingTestType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ import java.util.Optional;
 
 @Repository
 public interface TypingTestRepository extends JpaRepository<TypingTest, Long> {
-
+    Page<TypingTest> findByType(TypingTestType type, Pageable pageable);
+    Page<TypingTest> findAll(Specification<TypingTest> spec, Pageable pageable);
 }
