398cd9e69c980cca2a3971aa8868c9f6c861f563
Jacob Ferrell <michaeljacobferrell@gmail.com>
2024-10-05 10:26:39 -0400

    adds endpoint for searching tests

--- src/main/java/com/jacobferrell/Key2Glory/controller/TypingTestController.java
+++ src/main/java/com/jacobferrell/Key2Glory/controller/TypingTestController.java
@@ -1,9 +1,11 @@
 package com.jacobferrell.Key2Glory.controller;
 
 import com.jacobferrell.Key2Glory.dto.TypingTestDTO;
+import com.jacobferrell.Key2Glory.dto.TypingTestSortAndFilterDTO;
 import com.jacobferrell.Key2Glory.model.TypingTest;
+import com.jacobferrell.Key2Glory.model.SortTypes;
+import com.jacobferrell.Key2Glory.model.OrderTypes;
 import com.jacobferrell.Key2Glory.service.TypingTestService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,6 +27,18 @@ public class TypingTestController {
     @GetMapping("/public/typing-test/{id}/text")
     public ResponseEntity<String> getTypingTestText(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.getText(id));
+    }
+    @GetMapping("/public/typing-test")
+    public ResponseEntity<?> getTypingTests(
+        @RequestParam(name = "creators", required = false) String creators,
+        @RequestParam(name = "types", required = false) String types,
+        @RequestParam(name = "sort", required = false) SortTypes sort,
+        @RequestParam(name = "order", required = false) OrderTypes order,
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        TypingTestSortAndFilterDTO params = new TypingTestSortAndFilterDTO(creators, types, sort, order, page, size);
+        return ResponseEntity.ok().body(service.getTests(params));
     }
     @PostMapping("/private/typing-test")
     public ResponseEntity<?> createTypingTest(
